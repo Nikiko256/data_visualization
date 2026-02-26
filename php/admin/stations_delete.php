cd2797e85b44d16c8be32fc4a49639991a2b65a4
Niki Kolete <58658945+Nikiko256@users.noreply.github.com>
2026-02-27 00:39:26 +0200

    Update stations_delete.php

--- php/admin/stations_delete.php
+++ php/admin/stations_delete.php
@@ -7,12 +7,6 @@ if ($s_id === '') fail(422, "Missing s_id");
 
 $cnx = db();
 
-// delete nodes metadata too (optional but tidy)
-$stmt = mysqli_prepare($cnx, "DELETE FROM station_nodes WHERE s_id=?");
-mysqli_stmt_bind_param($stmt, 's', $s_id);
-mysqli_stmt_execute($stmt);
-mysqli_stmt_close($stmt);
-
 // delete station
 $stmt = mysqli_prepare($cnx, "DELETE FROM stations WHERE s_id=?");
 mysqli_stmt_bind_param($stmt, 's', $s_id);
