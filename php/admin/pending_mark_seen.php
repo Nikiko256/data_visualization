f3b153de5865ba5e58433631fbfbfa61aff40ce6
Niki Kolete <58658945+Nikiko256@users.noreply.github.com>
2026-02-27 03:01:35 +0200

    constructing notification seen

---
+++ php/admin/pending_mark_seen.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/_db.php';
+$in = read_json();
+$s_id = trim($in['s_id'] ?? '');
+if ($s_id === '') fail(422, "Missing s_id");
+
+$cnx = db();
+$stmt = mysqli_prepare($cnx, "UPDATE pending_stations SET seen=1 WHERE s_id=?");
+mysqli_stmt_bind_param($stmt, 's', $s_id);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+
+ok();
+?>
